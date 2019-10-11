6672fb61c005e3d1e90ba0a7fb1755732307f4fa
Daria Sokolova <dasha-inet@mail.ru>
2019-10-11 22:41:15 +0400

    feat: implementation of algoruthm

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = 
+function check(str, bracketsConfig) {
+  //создадим массив с конфигурациями и с помощью join переведем его содержимое в строки
+    let myConf = [];
+    for(let index=0; index<bracketsConfig.length; index++){
+      myConf.push(bracketsConfig[index].join(''));
+    }
+//убираем из строки парные скобки пока не получим результат
+    let prevLength = str.length+1;
+    for( ;prevLength>str.length; ){
+        prevLength = str.length;
+        for(let i=0; i<myConf.length;i++){
+          str = str.replace(myConf[i],'');
+        }             
+    }
+    if(prevLength===0){
+      return true;      
+    }else{
+      return false;
+    }  
 }
+
